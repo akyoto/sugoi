7fe6a6454cb8c9c35577d006c46a7c2006f626ad
Eduard Urbach <e.urbach@gmail.com>
2015-03-17 19:40:05 +0900

    Initial commit

---
+++ do.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Imports
+from subprocess import call
+
+# ls
+call(["ls", "-la"])
+
+# Riak
+riakUrl = "http://s3.amazonaws.com/downloads.basho.com/riak/2.0/2.0.5/ubuntu/trusty/riak_2.0.5-1_amd64.deb"
