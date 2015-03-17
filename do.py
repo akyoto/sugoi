bef9b1c7498c1addd14c2b43a6d36d43eb14ce08
Eduard Urbach <e.urbach@gmail.com>
2015-03-17 20:01:59 +0900

    Command dispatching

--- do.py
+++ do.py
@@ -2,9 +2,19 @@
 
 # Imports
 from subprocess import call
+import sys
 
-# ls
-call(["ls", "-la", "--color"])
+cmd = sys.argv[1]
 
-# Riak
-riakUrl = "http://s3.amazonaws.com/downloads.basho.com/riak/2.0/2.0.5/ubuntu/trusty/riak_2.0.5-1_amd64.deb"
+class Commands:
+	def ls(args):
+		call(["ls", "-la", "--color"])
+		
+	def install(args):
+		riakUrl = "http://s3.amazonaws.com/downloads.basho.com/riak/2.0/2.0.5/ubuntu/trusty/riak_2.0.5-1_amd64.deb"
+		print(args)
+		print(riakUrl)
+
+# Run the command
+commands = Commands()
+commands[cmd](args[2:])
