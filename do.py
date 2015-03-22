6346ae4e10e0360c624c774d6f1c5c4ca0ef4a26
Eduard Urbach <e.urbach@gmail.com>
2015-03-22 15:32:36 +0900

    Added riak tuning parameters

--- do.py
+++ do.py
@@ -13,6 +13,22 @@ class Commands:
 	def ls(args):
 		call(["ls", "-la", "--color"] + args)
 		
+	def tune(args):
+		def sysctl(param):
+			call(["sudo", "sysctl", param])
+		
+		sysctl("vm.swappiness=0")
+		sysctl("net.ipv4.tcp_max_syn_backlog=40000")
+		sysctl("net.core.somaxconn=40000")
+		sysctl("net.core.wmem_default=8388608")
+		sysctl("net.core.rmem_default=8388608")
+		sysctl("net.ipv4.tcp_sack=1")
+		sysctl("net.ipv4.tcp_window_scaling=1")
+		sysctl("net.ipv4.tcp_fin_timeout=15")
+		sysctl("net.ipv4.tcp_keepalive_intvl=30")
+		sysctl("net.ipv4.tcp_tw_reuse=1")
+		sysctl("net.ipv4.tcp_moderate_rcvbuf=1")
+		
 	def info(args):
 		print("Kernel:")
 		call(["uname", "-a"])
@@ -42,10 +58,16 @@ class Commands:
 
 # Package installations
 class Install:
-	def riak(args):
+	def riak(args = []):
+		# Dependencies
+		Install.make()
+		
 		call(["git", "clone", "git://github.com/basho/riak.git"])
 		os.chdir("./riak")
 		call(["make", "rel"])
+		
+	def make(args = []):
+		call(["sudo", "apt-get", "install", "make"])
 
 # Help
 def showHelp():
